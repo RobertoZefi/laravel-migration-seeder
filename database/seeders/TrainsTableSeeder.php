f96af0dd3b26abf448ef1171f7cd9a03de3cefe6
RobertoZefi <zefi.roberto2001@gmail.com>
2023-04-13 17:45:51 +0200

    FEAT es. done

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Seeder;
 use App\Models\Train;
 use Faker\Generator as Faker;
 
-class TrainTableSeeder extends Seeder
+class TrainsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -17,13 +17,17 @@ class TrainTableSeeder extends Seeder
     public function run(Faker $faker)
     {
         $azienda=['Trenitalia SpA', 'Trentino Trasporti Esercizio Srl', 'Trenord SpA', 'Impresa Ferroviaria Italiana'];
+        
 
         for($i = 0; $i<100; $i++){
+            $new_train = new Train();
+            //$orario_di_partenza = $faker->dateTime();
+
             $new_train->azienda = $faker->randomElement($azienda);
             $new_train->stazione_di_partenza = $faker->city();
             $new_train->stazione_di_arrivo = $faker->city();
             $new_train->orario_di_partenza = $faker->dateTime();
-            $new_train->orario_di_partenza = $faker->dateTime();
+            $new_train->orario_di_arrivo = $faker->dateTimeInInterval($new_train->orario_di_partenza, '+10 hour' );
             $new_train->codice_treno = $faker->bothify('########');
             $new_train->numero_carrozze = $faker->numberBetween(1,5);
             $new_train->in_orario = $faker->randomElement([0, 1]);
